7c73ae5ee4b767e15f5352e609a3fa8df36571ab
CLOVIS SFEIR <clovis.sfeir.cs@gmail.com>
2025-04-08 14:44:32 +0200

    franco

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Francofolies App',
+      title: 'Francofolies Appdfdf',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
